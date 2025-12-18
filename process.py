4ed6ed92b24adba2abf6f75d30da8d9049da8ce1
Jaakko Saaristo <jaakko.saaristo@gmail.com>
2025-12-18 08:46:48 +0000

    attempting to fix logging

--- process.py
+++ process.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import pika, sys, os, yaml, logging, time, random
+import pika, sys, os, yaml, time, random
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,19 +12,19 @@ def main():
     if profile is None and os.getenv('processing_profile') is not None:
         profile = os.getenv('processing_profile')
     if profile is None:
-        print("Please set either processing_profile environment variable or provide the profile name as the second argument")
+        print("Please set either processing_profile environment variable or provide the profile name as the second argument", flush=True)
         exit(1)
     
     rabbitmq_username = os.getenv('rabbitmq_username')
     rabbitmq_password = os.getenv('rabbitmq_password')
     if not rabbitmq_password or not rabbitmq_username:
-        print("Please set rabbitmq_username and rabbitmq_password env vars")
+        print("Please set rabbitmq_username and rabbitmq_password env vars", flush=True)
         exit(1)
 
-    print("profile: " + profile)
-    print("rabbitmq host: " + os.getenv('rabbitmq_host'))
-    print("rabbitmq port: " + os.getenv('rabbitmq_port'))
-    print("rabbitmq user: " + os.getenv('rabbitmq_username'))
+    print("profile: " + profile, flush=True)
+    print("rabbitmq host: " + os.getenv('rabbitmq_host'), flush=True)
+    print("rabbitmq port: " + os.getenv('rabbitmq_port'), flush=True)
+    print("rabbitmq user: " + os.getenv('rabbitmq_username'), flush=True)
 
     with open('config.yaml') as f:
         config = yaml.safe_load(f)
@@ -32,7 +32,7 @@ def main():
     do_read='read_queue' in config[profile]
     do_write='write_queue' in config[profile]
 
-    print("[x] Creating rabbitmq connections using given credentials...")
+    print("[x] Creating rabbitmq connections using given credentials...", flush=True)
 
     consumeConnection = pika.BlockingConnection(pika.ConnectionParameters(
         host=os.getenv('rabbitmq_host'), port=os.getenv('rabbitmq_port'), credentials=pika.PlainCredentials(
@@ -58,14 +58,8 @@ def main():
     )
     statusChannel = statusConnection.channel()  
 
-    logger = logging.getLogger(profile)
-    logdir = os.path.dirname(config[profile]['logfile'])
-    if not os.path.exists(logdir):
-        os.makedirs(logdir)
-    logging.basicConfig(filename=config[profile]['logfile'], level=logging.INFO)
-
     def callback(ch, method, properties, body):
-        logger.info(body)
+        print(body, flush=True)
         json = json.loads(body)
         time.sleep(random.randrange(config[profile]['delay_min'], config[profile]['delay_max']))
         if do_write:
@@ -74,7 +68,7 @@ def main():
 
     if do_read:
         consumeChannel.basic_consume(queue=config[profile]['read_queue'], on_message_callback=callback, auto_ack=True)
-        print(f"[x] Processor {profile} consuming {config[profile]['read_queue']} and writing to {config[profile]['write_queue']} and {config['common']['status_queue']}")
+        print(f"[x] Processor {profile} consuming {config[profile]['read_queue']} and writing to {config[profile]['write_queue']} and {config['common']['status_queue']}", flush=True)
         consumeChannel.start_consuming()
 
 if __name__ == '__main__':
