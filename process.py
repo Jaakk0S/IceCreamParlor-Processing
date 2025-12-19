4bd7f8d43f02df1465a0c12af6e5a30397e873fa
Jaakko Saaristo <jaakko.saaristo@gmail.com>
2025-12-19 09:07:23 +0000

    bugfix

--- process.py
+++ process.py
@@ -60,12 +60,13 @@ def main():
 
     def consumer_callback(ch: pika.channel.Channel, method: pika.spec.Basic.Deliver, properties: pika.spec.BasicProperties, consumedObjectBody: bytes):
         jsonStr = consumedObjectBody.decode().replace("'", '"')
-        print("Received object: " + jsonStr, flush=True)
+        print("Received string: " + jsonStr, flush=True)
         jsonObj = json.loads(jsonStr)
-        time.sleep(random.randrange(config[profile]['delay_min'], config[profile]['delay_max']))
+        print("Received object: " + jsonObj, flush=True)
+        #time.sleep(random.randrange(config[profile]['delay_min'], config[profile]['delay_max']))
         if do_write:
             produceChannel.basic_publish(exchange='', routing_key=config[profile]['write_queue'], body=consumedObjectBody)
-        statusChannel.basic_publish(exchange='', routing_key=config['common']['status_queue'], body="{ 'id' : '{0}', 'status': '{1}' }".format(jsonObj['id'], jsonObj['status']))
+        statusChannel.basic_publish(exchange='', routing_key=config['common']['status_queue'], body="{ 'id' : '{0}', 'status': '{1}' }".format(jsonObj["id"], jsonObj["status"]))
 
     if do_read:
         consumeChannel.basic_consume(queue=config[profile]['read_queue'], on_message_callback=consumer_callback, auto_ack=True)
